2cfe67306836ef8a2dc41e3e48c78bcfabe3d41e
wdlive24 <80212114+wdlive24@users.noreply.github.com>
2021-03-07 12:40:08 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -39,26 +39,22 @@ class FollowerBot():
     def AcceptFollowers(self):
         self.browser.get("https://www.instagram.com/accounts/activity/?followRequests")
 
-        try:
-            anfragen = self.WaitForObjects(By.CLASS_NAME, "sqdOP.L3NKy.y3zKF")
-
-            follow_buttons = []
-            #wir filtern die bestätigen schnell
-            for x in anfragen:
-                if x.text == "Bestätigen":
-                    follow_buttons.append(x)
-            anfragen_count = 0
-            for anfrage in follow_buttons:
-                anfrage.click()
-                anfragen_count +=1    
-                time.sleep(1)
-
-            print("{} neue Follower".format(anfragen_count))
-
+        try: 
+            requests = self.WaitForObject(By.CLASS_NAME, "sqdOP.L3NKy.y3zKF")
+            
+            requests_count = 0
+            for request in requests:
+                in request.text = "Confirm"
+                     request.click()
+                     request_count +=1
+                    
+            time.sleep(2)
+            print("{} new Follower".format(requests_count))
+                
         except selenium.common.exceptions.TimeoutException:
-            print("Keine neuen Anfragen....")
-
-bot = FollowerBot("DeinUsername", "DeinPassword")
+            print("No new Requests....") 
+       
+bot = FollowerBot("login.Username"(), "login.Password"())
 
 bot.login()
 
